104041dbcec7e860d432193fa26a791f652c2100
f-lab-hubert <hubert.bear@kakaomobility.com>
2021-07-11 22:27:36 +0900

    init project

---
+++ src/main/java/org/flab/reservation/ReservationDelegatorApplication.java
@@ -0,0 +1,13 @@
+package org.flab.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationDelegatorApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationDelegatorApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/org/flab/reservation/ReservationDelegatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.flab.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationDelegatorApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
