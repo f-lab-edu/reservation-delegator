31c725497a588bca48c7be8f411464bac26dbb08
sunny <parks38@uw.edu>
2021-07-21 19:57:22 +0900

    feat: jpa 환경 설정

--- src/main/java/org/flab/reservation/config/JpaConfig.java
+++ src/main/java/org/flab/reservation/config/JpaConfig.java
@@ -1,11 +1,9 @@
 package org.flab.reservation.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableJpaRepositories("org.flab.reservation.repository")
 @EnableTransactionManagement
 public class JpaConfig {
 
